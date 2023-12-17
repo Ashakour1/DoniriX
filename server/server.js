d395e8f5cea02ea4479a359d6de5f18616d679ca
Ashakuur1 <engshakrayare114@gmail.com>
2023-12-17 21:55:42 +0300

    `Setup`

--- server/server.js
+++ server/server.js
@@ -1,23 +1,19 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to my server",
+  });
+});
 
-app.get('/',(req,res) => {
-    res.json({
-        message: "Welcome to my server"
-    })
-})
-
-
-app.listen(PORT , () => {
-    console.log("Server is running on PORT: ", PORT);
-})
+app.listen(PORT, () => {
+  console.log("Server is running on PORT: ", PORT);
+});

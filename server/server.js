26f6bb3c1f279dd17104150360e180c79a275905
Ashakuur1 <engshakrayare114@gmail.com>
2023-12-17 21:36:58 +0300

    Setub `Server`

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/',(req,res) => {
+    res.json({
+        message: "Welcome to my server"
+    })
+})
+
+
+app.listen(PORT , () => {
+    console.log("Server is running on PORT: ", PORT);
+})

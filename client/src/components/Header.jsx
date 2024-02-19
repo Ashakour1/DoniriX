f15ad74659dafaa191355f5ad5f01e82fb9e0bb8
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-19 21:30:11 +0300

    Refactor Header component styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className=" text-green-600 w-full fixed top-0">
-      <div className=" max-w-[1040px] mx-auto flex justify-between items-center relative ">
+    <header className=" text-green-600  p-2 ">
+      <div className=" max-w-[1040px] mx-auto flex justify-between items-center  ">
         <div>
           <h1 className="text-2xl font-bold">Life Cord</h1>
         </div>
@@ -15,7 +15,10 @@ const Header = () => {
           </ul>
         </div>
         <div>
-          <a className="bg-green-600 font-bold text-white py-2 items-center px-4 rounded-md" href="">
+          <a
+            className="bg-green-600 font-bold text-white py-2 items-center px-4 rounded-md"
+            href=""
+          >
             Donate Now
           </a>
         </div>

0b62caa9bc60f69471a485406f3a9dcd42e4f727
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-19 17:14:51 +0300

    Add Header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className=" text-green-600">
+      <div className=" max-w-[1040px] mx-auto flex justify-between items-center">
+        <div>
+          <h1 className="text-2xl font-bold">Life Cord</h1>
+        </div>
+        <div>
+          <ul className="flex font-bold">
+            <li className="p-4">Home</li>
+            <li className="p-4">Contact</li>
+            <li className="p-4">Become partner</li>
+          </ul>
+        </div>
+        <div>
+          <a className="bg-green-600 font-bold text-white py-2 items-center px-4 rounded-md" href="">
+            Donate Now
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

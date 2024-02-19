25440f3eb55a2c1f3160a6f0fc0d021a30759a33
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-19 22:40:04 +0300

    Update header navigation menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { TiThMenuOutline } from "react-icons/ti";
+import { BiMenuAltRight, BiMenuAltLeft } from "react-icons/bi";
 
 const Header = () => {
+  const [navIsOpen, setNavIsOpen] = React.useState(false);
+
+  const openNav = () => {
+    setNavIsOpen(!navIsOpen);
+  };
   return (
     <header className=" text-green-600  p-2 ">
       <div className=" max-w-[1040px] mx-auto flex justify-between items-center  ">
@@ -24,24 +29,35 @@ const Header = () => {
           </a>
         </div>
       </div>
-      <button className="md:hidden absolute top-4 right-4">
-        <TiThMenuOutline className="text-2xl" />
-      </button>
 
-      <div className="md:hidden absolute top-10 right-0 bg-black h-58  text-white">
-        <ul className="font-bold">
-          <li className="p-4">Home</li>
-          <li className="p-4">Contact</li>
-          <li className="p-4">Become partner</li>
-          <li className="p-4">
-            <a
-              className="bg-green-600 font-bold text-white py-2 items-center px-4 rounded-md"
-              href=""
-            >
-              Donate Now
-            </a>
-          </li>
-        </ul>
+      <div onClick={openNav} className="md:hidden block">
+        {navIsOpen ? (
+          <button className="md:hidden absolute top-4 right-4">
+            <BiMenuAltLeft className="text-2xl" />
+          </button>
+        ) : (
+          <button className="md:hidden absolute top-4 right-4">
+            <BiMenuAltRight className="text-2xl" />
+          </button>
+        )}
+
+        {navIsOpen ? (
+          <div className="md:hidden absolute top-10 right-0 bg-black h-58  text-white">
+            <ul className="font-bold">
+              <li className="p-4">Home</li>
+              <li className="p-4">Contact</li>
+              <li className="p-4">Become partner</li>
+              <li className="p-4">
+                <a
+                  className="bg-green-600 font-bold text-white py-2 items-center px-4 rounded-md"
+                  href=""
+                >
+                  Donate Now
+                </a>
+              </li>
+            </ul>
+          </div>
+        ) : null}
       </div>
     </header>
   );

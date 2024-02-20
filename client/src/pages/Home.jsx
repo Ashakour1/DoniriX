48ad9a3a3dda8d6e23c6eccbb45fac8dee54ca59
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-20 20:08:41 +0300

    Refactor modal functionality in Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Home = () => {
+  const modal = React.useRef(null);
+
+  const showModal = () => {
+    if (!modal.current) {
+      return;
+    }
+    modal.current.showModal();
+  };
   return (
     <main className="max-w-[1040px] mx-auto mt-12">
       <div className="flex justify-between gap-20">
@@ -18,7 +26,7 @@ const Home = () => {
           </p>
           <div className="flex gap-4">
             <button
-              onClick={() => document.getElementById("my_modal_2").showModal()}
+              onClick={showModal}
               className="bg-black text-green-400 font-bold py-2 px-4 rounded mt-4"
             >
               donate Now
@@ -38,16 +46,18 @@ const Home = () => {
         </div>
       </div>
 
-      <dialog id="my_modal_2" className="modal">
-  <div className="modal-box">
-    <form method="dialog">
-      {/* if there is a button in form, it will close the modal */}
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click on ✕ button to close</p>
-  </div>
-</dialog>
+      <dialog id="my_modal_2" className="modal" ref={modal}>
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        </div>
+      </dialog>
     </main>
   );
 };

41404b587125b2904a3b949a934a2645b99b23c4
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-21 22:29:48 +0300

    Update modal form in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -44,13 +44,16 @@ const Home = () => {
       <dialog id="my_modal_2" className="modal" ref={modal}>
         <div className="modal-box">
           <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
               ✕
             </button>
+            <div>
+              <h1 className="font-bold text-2xl">Donate Blood</h1>
+              <p className="text-md pt-2">
+                Please fill in the form below to donate blood
+              </p>
+            </div>
           </form>
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click on ✕ button to close</p>
         </div>
       </dialog>
     </main>

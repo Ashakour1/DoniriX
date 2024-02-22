f04a97f32181961ce93f9e917b9f59c795bbed98
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-23 01:13:43 +0300

    Update server.js with CORS configuration

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
 import Donars from "./routes/DonarRoutes.js";
 import Admin from "./routes/AdminRoutes.js";
 import ErrorHandler from "./middlewares/errorMiddleware.js";
-
-const app = express();
+import dotenv from "dotenv";
+import cors from "cors"
 dotenv.config();
+const app = express();
+
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}))
+
 const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

7a68203e36387c4f78026c2992c2212127f0b77f
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-23 01:13:56 +0300

    Add blood donation form and submit functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { showModal } from "../utils/modal";
+import { publicRequest } from "../requestMethod";
+import axios from "axios";
 
 const Home = () => {
   const modal = React.useRef(null);
+
+  const [formData, setFormData] = useState({
+    fullname: "",
+    email: "",
+    phone: "",
+    age: "",
+    weight: "",
+    address: "",
+    motherNumber: "",
+    bloodType: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/donar/",
+        formData
+      );
+
+      console.log(response.data);
+
+     if(formData){
+      if(response.data && response.data.message){
+        alert(response.data.message)
+      }else{
+        alert("An error occurred. Please try again later.");
+      }
+     }
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.message) {
+        alert(error.response.data.message);
+      } else {
+        alert("An error occurred. Please try again later.");
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   return (
     <main className="max-w-[1040px] mx-auto mt-12">
@@ -43,16 +87,112 @@ const Home = () => {
 
       <dialog id="my_modal_2" className="modal" ref={modal}>
         <div className="modal-box">
-          <form method="dialog">
+          <form method="dialog" className="mx-3" onSubmit={handleSubmit}>
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
               ✕
             </button>
-            <div>
+            <div className="">
               <h1 className="font-bold text-2xl">Donate Blood</h1>
               <p className="text-md pt-2">
                 Please fill in the form below to donate blood
               </p>
+              <div className="flex justify-between mt-4">
+                <div>
+                  <label className="block text-sm font-bold">Name</label>
+                  <input
+                    type="text"
+                    className="w-52 p-2 border border-gray-300 rounded mt-2"
+                    name="fullname"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-bold">Email</label>
+                  <input
+                    type="email"
+                    className="w-52 p-2 border border-gray-300 rounded mt-2"
+                    name="email"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="flex justify-between mt-2 ">
+                <div>
+                  <label className="block text-sm font-bold">Phone</label>
+                  <input
+                    type="Number"
+                    className="w-52 p-2 border border-gray-300 rounded mt-2"
+                    name="phone"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-bold">Age</label>
+                  <input
+                    type="number"
+                    className="w-52 p-2 border border-gray-300 rounded mt-2"
+                    name="age"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="flex justify-between mt-2 ">
+                <div>
+                  <label className="block text-sm font-bold">Weight</label>
+                  <input
+                    type="Number"
+                    className="w-52 p-2 border border-gray-300 rounded mt-2"
+                    name="weight"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-bold">Address</label>
+                  <input
+                    type="text"
+                    className="w-52 p-2 border border-gray-300 rounded mt-2"
+                    name="address"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="flex justify-between mt-2 ">
+                <div>
+                  <label className="block text-sm font-bold">
+                    Mother Number
+                  </label>
+                  <input
+                    type="Number"
+                    className="w-52 p-2 border border-gray-300 rounded mt-2"
+                    name="motherNumber"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="">
+                  <label htmlFor="" className="block text-sm font-bold">
+                    Blood Type
+                  </label>
+                  <select
+                    name="bloodType"
+                    id=""
+                    className="w-52  p-2 border border-gray-300 rounded mt-2"
+                    onChange={handleChange}
+                  >
+                    <option value="A+">A+</option>
+                    <option value="A-">A-</option>
+                    <option value="B+">B+</option>
+                    <option value="B-">B-</option>
+                    <option value="O+">O+</option>
+                    <option value="O-">O-</option>
+                    <option value="AB+">AB+</option>
+                    <option value="AB-">AB-</option>
+                  </select>
+                </div>
+              </div>
             </div>
+            <button className="w-full mt-4 bg-green-600 p-2.5 rounded text-white font-bold">
+              Submit
+            </button>
           </form>
         </div>
       </dialog>

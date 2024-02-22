9e6c8d8bda99ee78ec14fc07a7e72cbdd546f915
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-22 18:14:53 +0300

    Add routing and 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import NotFound from "./components/NotFound";
+
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={
+          <>
+          <Home/>
+          </>
+        }/>
+        <Route path="*" element={<NotFound/>}/>
+       
+      </Routes>
+    </Router>
+      
     </>
   );
 }

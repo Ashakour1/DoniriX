5b2a40ba2fe1a6b0eb12b5a806af745a645c46bd
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-25 22:19:09 +0300

    Refactor Contact page layout and add contact form

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -2,16 +2,68 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <main className="max-w-[1040px] mx-auto mt-12">
-      <div>
-        <div className="max-w-[600px] flex flex-col gap-5 ">
-          <h1 className="text-3xl font-bold"><span className="text-green-600">LifeCord</span> Contact</h1>
+    <main className="max-w-[1040px] mx-auto mt-5">
+      <div className=" md:flex-row flex-col flex">
+        <div className="max-w-[550px] flex flex-col gap-5 mt-4 md:px-0 px-4">
+          <h1 className="text-3xl font-bold">
+            <span className="text-green-600">LifeCord</span> Contact
+          </h1>
           <p className="text-2xl">
             blood donation website. Together, we can elevate our collective
             influence and offer unparalleled experiences to donors, recipients,
             and healthcare providers. Join hands with us and be a part of this
             noble cause.
           </p>
+        </div>
+        <div className="p-5 w-full">
+          <form>
+            <div className="flex flex-col">
+              <label htmlFor="" className="font-bold">
+                Name <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="mt-2 border border-gray-500 p-2 rounded"
+                type="text"
+                placeholder="enter your name"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="" className="font-bold">
+                Email <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="mt-2 border border-gray-500 p-2 rounded"
+                type="text"
+                placeholder="enter your name"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="" className="font-bold">
+                Subject <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="mt-2 border border-gray-500 p-2 rounded"
+                type="text"
+                placeholder="enter your name"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="" className="font-bold">
+                Message <span className="text-red-500">*</span>
+              </label>
+              <textarea
+                className="border border-gray-500 rounded"
+                name=""
+                id=""
+                cols="30"
+                rows="5"
+              ></textarea>
+            </div>
+
+            <button className="w-full bg-green-500 text-white rounded mt-4 p-2">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </main>

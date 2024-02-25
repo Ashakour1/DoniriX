3b8ae86ecd48bc562c5edcfe34e5ad35f1c9eea9
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-25 14:52:09 +0300

    Update Contact page layout and content

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <main>
-      <h1>Contact</h1>
+    <main className="max-w-[1040px] mx-auto mt-12">
+      <div>
+        <div className="max-w-[600px] flex flex-col gap-5 ">
+          <h1 className="text-3xl font-bold"><span className="text-green-600">LifeCord</span> Contact</h1>
+          <p className="text-2xl">
+            blood donation website. Together, we can elevate our collective
+            influence and offer unparalleled experiences to donors, recipients,
+            and healthcare providers. Join hands with us and be a part of this
+            noble cause.
+          </p>
+        </div>
+      </div>
     </main>
   );
 };

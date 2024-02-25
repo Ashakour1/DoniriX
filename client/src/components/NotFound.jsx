fbe73765f6707d1a732b496395d3010149ae7670
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-25 22:18:41 +0300

    Update NotFound component to display the current page location

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { CiFaceSmile } from "react-icons/ci";
 const NotFound = () => {
+  const location = window.location.pathname.split("/");
+
   return (
     <div className="text-center flex flex-col items-center  ">
-      <h1 className="text-3xl font-bold">404 - Not Found</h1>
+      <h1 className="text-3xl font-bold">404 - Not Found {location} Page</h1>
       <p className="text-2xl">
         Sorry, the page you are looking for does not exist.
       </p>

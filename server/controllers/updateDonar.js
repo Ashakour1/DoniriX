86d3ab50d1ebe223a3349747b7d35ed025a79d11
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-01 16:41:43 +0300

    Refactor updateDonar controller

--- server/controllers/updateDonar.js
+++ server/controllers/updateDonar.js
@@ -1,50 +1,58 @@
-import asyncHandler from 'express-async-handler';
-import prisma from '../config/prisma.js';
+import asyncHandler from "express-async-handler";
+import prisma from "../config/prisma.js";
 
-export const updateDonar =  asyncHandler(async (req,res) => {
+export const updateDonar = asyncHandler(async (req, res) => {
+  // get donar id from params
+  const { id } = req.params;
 
-    // get donar id from params
-    const {id} = req.params;
+  // get donar data from body
+  const {
+    fullname,
+    email,
+    phone,
+    age,
+    weight,
+    address,
+    motherNumber,
+    bloodType,
+  } = req.body;
 
-    // get donar data from body
-    const {name,email,phone,age,weight,address,motherName,fatherName,bloodType} = req.body;
+  // check if donar exists
+  const donarExists = await prisma.donar.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
 
-    // check if donar exists
-    const donarExists = await prisma.donar.findUnique({
-        where:{
-            id : Number(id)
-        }
-    })
+  if (!donarExists) {
+    res.status(404);
+    throw new Error("Donar not exists");
+  }
+  // update donar
 
-    if(!donarExists){
-        res.status(404);
-        throw new Error("Donar not exists")
-    }
-    // update donar
+  const updatedDonar = await prisma.donar.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      fullname,
+      email,
+      phone,
+      age,
+      weight,
+      address,
+      motherName,
+      fatherName,
+      bloodType,
+      status: "Donated",
+    },
+  });
 
-    const updatedDonar = await prisma.donar.update({
-        where:{
-            id : Number(id)
-        },
-        data:{
-            name,
-            email,
-            phone,
-            age,
-            weight,
-            address,
-            motherName,
-            fatherName,
-            bloodType,
-            status : "Donated"
-        }
-    })
+  // return updated donar
 
-    // return updated donar
-
-    res.status(200).json({
-        success:true,
-        error:false,
-        message : "Donar updated successfully",
-    })
-})
+  res.status(200).json({
+    success: true,
+    error: false,
+    message: "Donar updated successfully",
+  });
+});

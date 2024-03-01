817b8794cb7246caa7cc212923cb740bf9fc1e85
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-01 19:37:50 +0300

    Add NextUIProvider to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,53 +7,56 @@ import { Toaster, toast } from "sonner";
 import About from "./pages/About";
 import Admin from "./pages/Admin/adminDashboard";
 import DonarList from "./pages/Admin/DonarList";
+import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   return (
     <>
-      <Toaster richColors position="top-center" />
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <Home />
-              </>
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <>
-                <Header />
-                <NotFound />
-              </>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <>
-                <Header />
-                <About />
-              </>
-            }
-          />
-          <Route
-            path="/contact"
-            element={
-              <>
-                <Header />
-                <Contact />
-              </>
-            }
-          />
-          <Route path="/auth/admin" element={<Admin />} />
-          <Route path="/donars" element={<DonarList />} />
-        </Routes>
-      </Router>
+      <NextUIProvider>
+        <Toaster richColors position="top-center" />
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Header />
+                  <Home />
+                </>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <>
+                  <Header />
+                  <NotFound />
+                </>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <>
+                  <Header />
+                  <About />
+                </>
+              }
+            />
+            <Route
+              path="/contact"
+              element={
+                <>
+                  <Header />
+                  <Contact />
+                </>
+              }
+            />
+            <Route path="/auth/admin" element={<Admin />} />
+            <Route path="/donars" element={<DonarList />} />
+          </Routes>
+        </Router>
+      </NextUIProvider>
     </>
   );
 }

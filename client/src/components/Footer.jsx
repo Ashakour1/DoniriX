fec7333928d6eae72cf17fb645d9c78eeb654190
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-07 22:09:16 +0300

    Add Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="bg-black">
+      <div className="max-w-[1040px] mx-auto">
+        <p className="text-white text-center">
+          Developed with ❤️ by{" "}
+          <Link className="underline" to="https://github.com/Ashakour1">Abdishakur</Link>{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

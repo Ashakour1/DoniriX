ef2d5982c42a1d25374f3b294603035b0e70c32f
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-08 17:49:25 +0300

    Refactor Footer component to improve code readability

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,8 +6,10 @@ const Footer = () => {
     <div className="bg-black">
       <div className="max-w-[1040px] mx-auto mt-20">
         <p className="text-white text-center">
-          Developed with ❤️ by{" "}
-          <Link className="underline" to="https://github.com/Ashakour1">Abdishakur</Link>{" "}
+          Developed with ❤️ by
+          <Link className="underline" to="https://github.com/Ashakour1">
+            Abdishakur
+          </Link>
         </p>
       </div>
     </div>

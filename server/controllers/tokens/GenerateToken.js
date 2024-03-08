8bbae3affae1388a868565158a222ef2238c437b
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-08 21:29:24 +0300

    Refactor generateToken function to return token and maxAge

--- server/controllers/tokens/GenerateToken.js
+++ server/controllers/tokens/GenerateToken.js
@@ -1,5 +1,9 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (id) => {
-    return jwt.sign({id} , process.env.JWT_SECRET , {expiresIn: "10d"}) 
-}
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "10d" });
+  const decodedToken = jwt.decode(token);
+  const maxAge = (decodedToken && decodedToken.exp * 1000) || 0; // Multiply by 1000 to convert seconds to milliseconds
+
+  return { token, maxAge };
+};

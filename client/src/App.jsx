897bb65f835475a9b290e6d3339f2d32e2419455
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-08 21:30:38 +0300

    Add Dashboard route to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Footer from "./components/Footer";
 
 import { NextUIProvider } from "@nextui-org/react";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -56,6 +57,7 @@ function App() {
               }
             />
             <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </Router>
       </NextUIProvider>

f9497e601dde824e7386ede32c93b6bf89e1b258
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-09 14:19:58 +0300

    Refactor routing in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Contact from "./pages/Contact";
 import { Toaster, toast } from "sonner";
@@ -16,50 +16,49 @@ function App() {
     <>
       <NextUIProvider>
         <Toaster richColors position="top-center" />
-        <Router>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Header />
-                  <Home />
-                  <Footer />
-                </>
-              }
-            />
-            <Route
-              path="*"
-              element={
-                <>
-                  <Header />
-                  <NotFound />
-                </>
-              }
-            />
-            <Route
-              path="/about"
-              element={
-                <>
-                  <Header />
-                  <About />
-                  <Footer />
-                </>
-              }
-            />
-            <Route
-              path="/contact"
-              element={
-                <>
-                  <Header />
-                  <Contact />
-                </>
-              }
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </Router>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <>
+                <Header />
+                <NotFound />
+              </>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <>
+                <Header />
+                <About />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <>
+                <Header />
+                <Contact />
+              </>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
       </NextUIProvider>
     </>
   );

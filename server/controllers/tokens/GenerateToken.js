b73b25f4052a6bae2c0939f167d95c68d5a74133
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-10 22:41:23 +0300

    Comment out generateToken function in GenerateToken.js

--- server/controllers/tokens/GenerateToken.js
+++ server/controllers/tokens/GenerateToken.js
@@ -1,9 +1,12 @@
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
-export const generateToken = (id) => {
-  const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "10d" });
-  const decodedToken = jwt.decode(token);
-  const maxAge = (decodedToken && decodedToken.exp * 1000) || 0; // Multiply by 1000 to convert seconds to milliseconds
+// export const generateToken = (id) => {
+//   const expiresIn = 7 * 24 * 60 * 60;
+//   const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+//     expiresIn: expiresIn,
+//   });
+//   const decodedToken = jwt.decode(token);
+//   const maxAge = (decodedToken && decodedToken.exp * 1000) || 0; // Multiply by 1000 to convert seconds to milliseconds
 
-  return { token, maxAge };
-};
+//   return { token, maxAge };
+// };

b0fa05984c75ce6ab74b728a1c4b23cf5fe4e97d
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-10 22:40:48 +0300

    Add user authentication to Dashboard component

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useUser } from "../hooks/useUser";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const { user } = useUser();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>
+        Welcome {user?.adminExists.name} to the dashboard. You are now logged
+        in.
+      </h1>
     </div>
   );
 };

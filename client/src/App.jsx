f93d04f24afe3aa80f64364519a6d18db7b0fb9c
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-10 22:40:21 +0300

    Update routing in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Contact from "./pages/Contact";
 import { Toaster, toast } from "sonner";
@@ -16,49 +16,58 @@ function App() {
     <>
       <NextUIProvider>
         <Toaster richColors position="top-center" />
-
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <Home />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <>
-                <Header />
-                <NotFound />
-              </>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <>
-                <Header />
-                <About />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path="/contact"
-            element={
-              <>
-                <Header />
-                <Contact />
-              </>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Header />
+                  <Home />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <>
+                  <Header />
+                  <NotFound />
+                </>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <>
+                  <Header />
+                  <About />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/contact"
+              element={
+                <>
+                  <Header />
+                  <Contact />
+                </>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/dashboard"
+              element={
+                <>
+                  <Header />
+                  <Dashboard />
+                </>
+              }
+            />
+          </Routes>
+        </Router>
       </NextUIProvider>
     </>
   );

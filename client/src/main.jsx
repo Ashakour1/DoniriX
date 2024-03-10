ed332f7218b09fbf14d5dd24eff0fdc21b230353
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-10 22:40:26 +0300

    Refactor BrowserRouter to use UserProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./hooks/useUser.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );

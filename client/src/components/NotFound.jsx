6794fadb115a0bac905a06bf480dd7035647be8b
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-12 23:41:13 +0300

    Fix typo in NotFound component

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
     <div className="text-center flex flex-col items-center  ">
       <h1 className="text-3xl font-bold">404 - Not Found {location} Page</h1>
       <p className="text-2xl">
-        Sorry, the page you are looking for does not exist.
+        Sorry, the page you are looking for does not exist..
       </p>
       <CiFaceSmile className="text-green-500 text-5xl" />
     </div>

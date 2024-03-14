46ac532b7c6d92eff5edd2931af2fb5c89be6145
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-14 21:18:18 +0300

    Add donar data retrieval to Dashboard component

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useUser } from "../hooks/useUser";
 import { useLocation, useNavigate } from "react-router-dom";
+import { publicRequest } from "../requestMethod";
 
 const Dashboard = () => {
   const { user } = useUser();
+  const [donars, setDonars] = useState();
 
-  console.log(user);
+  // console.log(user);
   const location = useLocation();
 
   const redirectTo = location.pathname;
-  console.log(redirectTo);
+  // console.log(redirectTo);
 
   const navigate = useNavigate();
 
@@ -19,13 +21,28 @@ const Dashboard = () => {
     }
   }, [user, navigate]);
 
+  useEffect(() => {
+    const getDonars = async () => {
+      try {
+        const response = await publicRequest.get("/donar");
+        setDonars(response.data.results.data.donars);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getDonars();
+  });
+
   return (
-    <div>
-      <h1>
-        Welcome {user?.adminExists.name} to the dashboard. You are now logged
-        in.
-      </h1>
-    </div>
+    <main className="max-w-[1200px] mx-auto px-4">
+      <div>
+        <h1>
+          Welcome {user?.adminExists.name} to the dashboard. You are now logged
+          in.
+        </h1>
+      </div>
+      
+    </main>
   );
 };
 

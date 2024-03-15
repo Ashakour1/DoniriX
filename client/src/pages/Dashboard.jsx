261a3f72d367fd0468abacb89d6a742bf79716a1
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-15 22:04:09 +0300

    Refactor Dashboard component layout and add recent donors table

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useUser } from "../hooks/useUser";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { publicRequest } from "../requestMethod";
 
 const Dashboard = () => {
@@ -60,12 +60,71 @@ const Dashboard = () => {
   }, [userData]);
 
   return (
-    <main className="max-w-[1200px] mx-auto px-4">
-      <div>
-        <h1>
-          Welcome {user?.adminExists.name} to the dashboard. You are now logged
-          in.
-        </h1>
+    <main>
+      <div className="bg-gray-100 min-h-screen pt-10">
+        <div className="flex">
+          <div className="flex-1 p-8 ml-64 ">
+            <div className="grid grid-cols-3 gap-6">
+              <div className=" bg-gray-500 p-6 rounded-lg shadow">
+                <h3 className="text-2xl font-bold text-white mb-2">
+                  {donars?.filter((donar) => donar.status === "finished").length}
+                </h3>
+                <p className="text-white">Donations</p>
+              </div>
+              <div className="bg-gray-500 p-6 rounded-lg shadow">
+                <h3 className="text-2xl font-bold text-white  mb-2">
+                  {donars?.filter((donar) => donar.status === "pending").length}
+                </h3>
+                <p className="text-white ">Pending</p>
+              </div>
+              <div className="bg-gray-500 p-6 rounded-lg shadow">
+                <h3 className="text-2xl font-bold text-white mb-2">
+                  {donars?.length}
+                </h3>
+                <p className="text-white ">Total</p>
+              </div>
+            </div>
+            <div className=" bg-gray-500 p-6 rounded-lg shadow mt-8">
+              <h2 className="text-2xl font-bold text-white  mb-4">
+                Recent Donors
+              </h2>
+              <div className="overflow-x-auto">
+                <table className="w-full table-auto">
+                  <thead>
+                    <tr>
+                      <th className="px-4 py-2 text-left text-white ">Name</th>
+                      <th className="px-4 py-2 text-left text-white">
+                        Blood Type
+                      </th>
+                      <th className="px-4 py-2 text-left text-white">Status</th>
+                      <th className="px-4 py-2 text-left text-white">
+                        Donation Date
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {donars?.slice(0, 5).map((donar) => (
+                      <tr className="border-b dark:border-gray-700" key={donar.id}>
+                        <td className="px-4 py-2 text-gray-900 dark:text-white">
+                          {donar.fullname}
+                        </td>
+                        <td className="px-4 py-2 text-gray-900 dark:text-white">
+                          {donar.bloodType}
+                        </td>
+                        <td className="px-4 py-2 text-gray-900 dark:text-white">
+                          {donar.status}
+                        </td>
+                        <td className="px-4 py-2 text-gray-900 dark:text-white">
+                         {donar.createdAt.slice(0, 10)}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );

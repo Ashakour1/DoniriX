2ddb3cd86e2ef673e420800df6e0574a5fc0a4aa
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-19 14:32:49 +0300

    Add DonarList component

---
+++ client/src/pages/DonarList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DonarList = () => {
+  return (
+    <div className='ml-72 text-black pt-20'>
+      <h1 className='text-3xl font-bold'>Donar List</h1>
+    </div>
+  )
+}
+
+export default DonarList

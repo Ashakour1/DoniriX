0bdc4985a5d05e6b73b8e4ae22ee2aba3b15208f
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-20 23:32:34 +0300

    Update RecentDonars-Table.jsx to display donar status with different styling

--- client/src/components/RecentDonars-Table.jsx
+++ client/src/components/RecentDonars-Table.jsx
@@ -13,7 +13,7 @@ const RecentDonars = ({donar}) => {
       {donar.bloodType}
     </td>
     <td className="px-4 py-2 text-gray-900 dark:text-white">
-      {donar.status}
+      {donar.status === "finished" ? (<span className="text-white bg-green-400 py-1 px-2 rounded">{donar.status}</span>) : <span className="px-2 py-1 rounded text-white bg-red-400">{donar.status}</span>}
     </td>
     <td className="px-4 py-2 text-gray-900 dark:text-white">
       {donar.createdAt.slice(0, 10)}

c78aa448de496f7853abbccdbe66d970bee06614
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-29 22:04:01 +0300

    Refactor About page layout

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,32 +3,12 @@ import React from "react";
 const About = () => {
   return (
     <main className="max-w-[1040px] mx-auto mt-5 min-h-screen">
-    <div className=" md:flex-row flex-col flex md:px-0 px-4 pb-4">
-      <div className="max-w-[550px] flex flex-col gap-5 mt-4">
-        <h1 className="text-3xl font-bold">
-          <span className="text-green-600">LifeCord</span> About
-        </h1>
-        <p className="text-xl">
-          blood donation  website Together, we can elevate our collective
-          influence and offer unparalleled experiences to donors, recipients,
-          and healthcare providers. Join hands with us and be a part of this
-          noble cause.
-        </p>
-        <h1 className="text-2xl font-bold">
-          <span className="text-green-600">LifeCord</span> About
-        </h1>
-        <p className="text-xl">
-          blood donation website Together, we can elevate our collective
-          influence and offer unparalleled experiences to donors, recipients,
-          and healthcare providers. Join hands with us and be a part of this
-          noble cause.
-        </p>
+      <div>
+        <div className="p-4">
+          <h1 className="text-green-600 underline">About Us</h1>
+        </div>
       </div>
-      <div className="pt-3 w-full">
-       <img  className="w-[600px] h-[300px] object-cover rounded" src="https://via.placeholder.com/640x360" alt="" />
-      </div>
-    </div>
-  </main>
+    </main>
   );
 };
 

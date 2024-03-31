3aee7e63bf8039129fbdfe5fed407b1a772bc490
Ashakuur1 <engshakrayare114@gmail.com>
2024-03-31 20:40:38 -0300

    Add About Us description

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,6 +6,9 @@ const About = () => {
       <div>
         <div className="p-4">
           <h1 className="text-green-600 underline">About Us</h1>
+          <p className="text-justify mt-4">
+            At <span className="text-green-600">LifeCord</span> we're dedicated to saving lives through the power of LifeCord. Our mission is to safeguard lives by facilitating the vital connection between compassionate donors and individuals in critical need of life-saving blood transfusions. We firmly believe that every single drop of blood holds the power to make a profound and positive impact in someone's life.
+          </p>
         </div>
       </div>
     </main>

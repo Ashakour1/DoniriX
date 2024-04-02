34dc6e21526ae79ed6997576d6e9c8eb8701acdf
Ashakuur1 <engshakrayare114@gmail.com>
2024-04-02 23:35:30 +0300

    Refactor RecentDonars-Table component

--- client/src/components/RecentDonars-Table.jsx
+++ client/src/components/RecentDonars-Table.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-
-const RecentDonars = ({donar}) => {
+import React from "react";
+import { Badge } from "@nextui-org/react";
+const RecentDonars = ({ donar }) => {
   return (
-    <tr
-    className="border-b dark:border-gray-700"
-    key={donar.id}
-  >
-    <td className="px-4 py-2 text-gray-900 dark:text-white">
-      {donar.fullname}
-    </td>
-    <td className="px-4 py-2 text-gray-900 dark:text-white">
-      {donar.bloodType}
-    </td>
-    <td className="px-4 py-2 text-gray-900 dark:text-white">
-      {donar.status === "finished" ? (<span className="text-white bg-green-400 py-1 px-2 rounded">{donar.status}</span>) : <span className="px-2 py-1 rounded text-white bg-red-400">{donar.status}</span>}
-    </td>
-    <td className="px-4 py-2 text-gray-900 dark:text-white">
-      {donar.createdAt.slice(0, 10)}
-    </td>
-  </tr>
-  )
-}
+    <tr className="border-b dark:border-gray-700" key={donar.id}>
+      <td className="px-4 py-2 text-gray-900 dark:text-white">
+        {donar.fullname}
+      </td>
+      <td className="px-4 py-2 text-gray-900 dark:text-white">
+        {donar.bloodType}
+      </td>
+      <td className="px-4 py-2 text-gray-900 dark:text-white">
+        {donar.status === "finished" ? (
+          <span className="text-white bg-green-500 py-1 px-2 rounded-full font-semibold">
+            {donar.status}
+          </span>
+        ) : (
+          <span className="px-2 py-1 rounded-full text-white bg-red-700 font-semibold">
+            {donar.status}
+          </span>
+        )}
+      </td>
+      <td className="px-4 py-2 text-gray-900 dark:text-white">
+        {donar.createdAt.slice(0, 10)}
+      </td>
+    </tr>
+  );
+};
 
-export default RecentDonars
+export default RecentDonars;

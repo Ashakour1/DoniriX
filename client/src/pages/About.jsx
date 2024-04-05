930c68b05572d364e726364af070baaa93723fb9
Ashakuur1 <engshakrayare114@gmail.com>
2024-04-05 23:50:49 +0300

    Remove lorem ipsum text from About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,23 +7,7 @@ const About = () => {
         <div className="p-4">
           <h1 className="text-green-600 underline">About Us</h1>
           <p className="text-justify mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            facilisi. Sed ac elit non nunc ultrices ultrices. Nullam nec
-            scelerisque sem. Sed nec libero sit amet orci ultricies
-            condimentum. Integer et libero auctor, dignissim nunc nec,
-            ullamcorper nunc. Sed nec libero sit amet orci ultricies
-            condimentum. Integer et libero auctor, dignissim nunc nec,
-            ullamcorper nunc. Sed nec libero sit amet orci ultricies
-            condimentum. Integer et libero auctor, dignissim nunc nec,
-            ullamcorper nunc. Sed nec libero sit amet orci ultricies
-            condimentum. Integer et libero auctor, dignissim nunc nec,
-            ullamcorper nunc. Sed nec libero sit amet orci ultricies
-            condimentum. Integer et libero auctor, dignissim nunc nec,
-            ullamcorper nunc. Sed nec libero sit amet orci ultricies
-            condimentum. Integer et libero auctor, dignissim nunc nec,
-            ullamcorper nunc. Sed nec libero sit amet orci ultricies
-            condimentum. Integer et libero auctor, dignissim nunc nec,
-            ullamcorper nunct.
+           
           </p>
         </div>
       </div>

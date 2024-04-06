fad21081fad16a657063d06d74030815f472b69c
Ashakuur1 <engshakrayare114@gmail.com>
2024-04-06 21:37:38 +0300

    Update link in Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
           </div>
         </div>
         <div className="hidden md:flex">
-          <Link className="p-2" to="/">
+          <Link className="p-2">
             <button onClick={handleModalToggle} className="bg-green-600 font-bold text-white py-2 items-center px-4 rounded-md">
               Donate Now
             </button>

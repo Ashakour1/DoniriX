dfd3cb480f63cbf91e2bda4465d87878034dbe6f
Ashakuur1 <engshakrayare114@gmail.com>
2024-04-06 19:36:48 +0300

    Add Spinner component

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Spinner } from "@nextui-org/react";
+
+const Loader = () => {
+  return <Spinner className="pt-20 pl-72 flex justify-center text-center" />;
+};
+
+export default Loader;

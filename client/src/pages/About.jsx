6ed58fa6f3476707dc02f06517cc11121bbbd511
Ashakuur1 <engshakrayare114@gmail.com>
2024-04-09 23:27:56 +0300

    Remove unnecessary text in About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,12 +6,7 @@ const About = () => {
       <div>
         <div className="p-4">
           <h1 className="text-green-600 underline">About Us</h1>
-          <p className="text-justify mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            malesuada, velit in varius fermentum, nunc nunc ultricies justo, ac
-            tincidunt sapien sapien in nunc. Integer nec purus nec nunc
-            fermentum ultricies. Sed nec nunc nec libero ultricies fermentum
-          </p>
+          <p className="text-justify mt-4"></p>
         </div>
       </div>
     </main>

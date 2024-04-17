6a7c49dedd1b04926ba79f9296947e81cd7bad37
Ashakuur1 <engshakrayare114@gmail.com>
2024-04-17 20:49:29 +0300

    Update NotFound component to use blue smiley face icon

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -10,7 +10,7 @@ const NotFound = () => {
       <p className="text-2xl">
         Sorry, the page you are looking for does not exist.
       </p>
-      <CiFaceSmile className="text-green-500 text-5xl" />
+      <CiFaceSmile className="text-blue-500 text-5xl" />
     </div>
   );
 };

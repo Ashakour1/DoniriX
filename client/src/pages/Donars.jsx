b0f3014b4ee4d6ac6e6a17b131872d30c8c9c585
Ashakuur1 <engshakrayare114@gmail.com>
2024-04-18 20:14:30 +0300

    Update Donars page to use blue smiley face icon

--- client/src/pages/Donars.jsx
+++ client/src/pages/Donars.jsx
@@ -5,13 +5,13 @@ const Donars = () => {
   return (
     <main className="max-w-[1040px] mx-auto min-h-screen">
       <div className="max-w-[800px] mx-auto flex flex-col items-center text-center">
-        <h1 className="text-green-600 text-3xl font-bold">Coming Soon!</h1>
+        <h1 className="text-blue-500 text-3xl font-bold">Coming Soon!</h1>
         <p className="my-3 text-2xl">
           We are working on this page and it will be available soon. Thanks for
           your understanding and patience as we make sure everything is just
           right.
         </p>
-        <FaRegFaceLaughWink className="text-green-600 text-5xl" />
+        <FaRegFaceLaughWink className="text-blue-500 text-5xl" />
       </div>
     </main>
   );

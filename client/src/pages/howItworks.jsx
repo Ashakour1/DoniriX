85ddd78f3f50f19feb713fe347c4167160a1c53e
Ashakuur1 <engshakrayare114@gmail.com>
2024-04-29 16:14:16 +0300

    Update padding in HowItworks page paragraph

--- client/src/pages/howItworks.jsx
+++ client/src/pages/howItworks.jsx
@@ -16,7 +16,7 @@ const HowItworks = () => {
           <h1 className="md:text-3xl text-2xl md:px-0 px-3 font-bold text-black">
             How does it Work?
           </h1>
-          <p className="pt-4 px-5 md:px-0">
+          <p className="pt-4 px-4 md:px-0">
             Here's How the process works for donors on LifeCord:
           </p>
         </div>

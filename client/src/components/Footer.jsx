4857458bce6914337a41193388407af8483884ff
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-01 00:27:22 +0300

    Update author name in Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <p className="text-white text-center">
           Developed with ❤️ by {""}
           <Link className="underline" to="https://github.com/Ashakour1">
-            A.shakur
+            A.shakur Mohe'd
           </Link>
         </p>
       </div>

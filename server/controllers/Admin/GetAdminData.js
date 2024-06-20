44630254dd9f0eab69ea65330277ae5e0379950e
Ashakuur1 <engshakrayare114@gmail.com>
2024-06-21 00:10:33 +0300

    Remove unused code in GetAdminData controller

--- server/controllers/Admin/GetAdminData.js
+++ server/controllers/Admin/GetAdminData.js
@@ -2,13 +2,6 @@ import asyncHandler from "express-async-handler";
 import prisma from "../../config/prisma.js";
 
 export const GetAdminData = asyncHandler(async (req, res) => {
-  const { id } = req.admin;
-
-  const loginAdmin = await prisma.admin.findUnique({
-    where: {
-      id: Number(id),
-    },
-  });
 
   /// get admin data
   const adminData = await prisma.admin.findMany({

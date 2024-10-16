b617f26851e2f2f7712762b033bbd88b656bfdde
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-17 00:44:10 +0300

    Add DonorFormPage component to render DonorForm

---
+++ client/src/pages/DonorFormPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DonorForm from "../components/DonorForm";
+
+const DonorFormPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 py-20 px-4">
+      <DonorForm />
+    </div>
+  );
+};
+
+export default DonorFormPage;

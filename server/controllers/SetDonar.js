a0cf8b55422d602d878db45161021654fc827849
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-17 00:42:41 +0300

    feat: Update setDonar controller to rename fields, add new properties, and improve validation

--- server/controllers/SetDonar.js
+++ server/controllers/SetDonar.js
@@ -2,57 +2,68 @@ import asyncHandler from "express-async-handler";
 import prisma from "../config/prisma.js";
 
 export const setDonar = asyncHandler(async (req, res) => {
-  // console.log(req.body);
+  console.log(req.body);
+
+  // fullname  String
+  // email     String   @unique
+  // phone     String
+  // age       Int
+  // weight    Float
+  // address  String
+  // nextOfkin String
+  // hp String
+  // bloodType     String
+  // status    String
   const {
-    fullname,
+    name,
     email,
     phone,
+    sex,
     age,
     weight,
     address,
-    motherNumber,
+    nextOfKin,
+    hp,
     bloodType,
+    amount,
   } = req.body;
 
+  // check if all fields are filled
   if (
-    !fullname ||
+    !name ||
     !email ||
     !phone ||
+    !sex ||
     !age ||
     !weight ||
     !address ||
-    !motherNumber ||
+    !nextOfKin ||
+    !hp ||
     !bloodType
   ) {
-    res.status(404);
-    throw new Error("Please fill all the fields");
+    res.status(400);
+    throw new Error("Please fill in all fields");
   }
 
   if (age < 18 || age > 50) {
     res.status(400);
     throw new Error("You are not eligible to donate blood");
-    
   }
- 
+
   if (weight < 50) {
     res.status(400);
     throw new Error("You are underweight");
   }
- 
 
   if (phone.length < 0) {
     res.status(400);
     throw new Error("Please enter a valid phone number");
   }
 
- 
-
-  if (motherNumber.length < 0) {
-    res.status(400);
-    throw new Error("Please enter a valid mother phone number");
-  }
-
-  
+  // if (motherNumber.length < 0) {
+  //   res.status(400);
+  //   throw new Error("Please enter a valid mother phone number");
+  // }
 
   // check if donar exists
   const donarExists = await prisma.donar.findUnique({
@@ -70,14 +81,17 @@ export const setDonar = asyncHandler(async (req, res) => {
   // create donar
   const Donar = await prisma.donar.create({
     data: {
-      fullname,
+      name,
       email,
       phone,
+      sex,
       age: Number(age),
       weight: Number(weight),
       address,
-      motherNumber,
+      nextOfKin,
+      hp,
       bloodType,
+      amount: Number(amount),
       status: "pending",
     },
   });

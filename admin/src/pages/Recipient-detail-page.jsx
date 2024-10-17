c27334af27c058453663968101460be0886194dc
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-17 23:04:40 +0300

    Add RecipientDetailPage component to display recipient details within a layout

---
+++ admin/src/pages/Recipient-detail-page.jsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/Layout/Layout";
+import { RecipientDetail } from "@/components/Recipient-detail";
+import React from "react";
+
+const RecipientDetailPage = () => {
+  return <div>
+    <Layout>
+        <RecipientDetail />
+    </Layout>
+  </div>;
+};
+
+export default RecipientDetailPage;

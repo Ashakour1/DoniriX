1304a62041c3f28b9f333ccde0aa3d5341b6bd09
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-19 22:08:15 +0300

    Add BloodTypes component to display and navigate blood type options

---
+++ admin/src/components/blood-types.jsx
@@ -0,0 +1,32 @@
+import { Droplet } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+const bloodTypes = ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"];
+
+export const BloodTypes = () => {
+  const navigate = useNavigate();
+//   console.log(navigate);
+  return (
+    <div className="max-w-[1400px] mx-auto bg-gray-100 flex flex-col items-center justify-center ">
+      <h1 className="text-4xl font-bold mb-8 text-gray-800">Blood Types</h1>
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4  w-full">
+        {bloodTypes.map((type) => (
+          <div
+            onClick={() => navigate(`/donors/${type}`)}
+            key={type}
+            className="
+              aspect-square rounded-lg shadow-md
+              flex flex-col items-center justify-center
+              bg-white text-gray-800
+            "
+          >
+            <Droplet className="mb-2 fill-red-500" size={32} />
+            <span className="text-2xl font-bold">{type}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BloodTypes;

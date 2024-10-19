2fe55778eedb240a26bc8e437bf9c1e09affeb7b
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-19 22:05:39 +0300

    Add Spinner component for loading indication

---
+++ admin/src/components/Spinner.jsx
@@ -0,0 +1,27 @@
+// components/Spinner.js
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="loader"></div>
+      <style jsx>{`
+        .loader {
+          border: 8px solid rgba(0, 0, 0, 0.1);
+          border-left-color: #3498db;
+          border-radius: 50%;
+          width: 50px;
+          height: 50px;
+          animation: spin 1s linear infinite;
+        }
+        @keyframes spin {
+          to {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Spinner;

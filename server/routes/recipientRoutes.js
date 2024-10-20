8003be932e9bac91e9e67cf1c970a31361ca3d7a
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-20 09:56:10 +0300

    Add authentication middleware to recipient routes for secure access

--- server/routes/recipientRoutes.js
+++ server/routes/recipientRoutes.js
@@ -5,13 +5,17 @@ import {
   deleteRecipient,
   updateRecipient,
 } from "../controllers/recipientController.js";
+
+import { AuthMiddleware } from "../middlewares/authMiddleware.js";
 import express from "express";
 const RecipientRouter = express.Router();
 
-RecipientRouter.route("/").get(getRecipients).post(createRecipient);
+RecipientRouter.route("/")
+  .get(AuthMiddleware, getRecipients)
+  .post(AuthMiddleware, createRecipient);
 RecipientRouter.route("/:id")
-  .get(getRecipientById)
-  .put(updateRecipient)
-  .delete(deleteRecipient);
+  .get(AuthMiddleware, getRecipientById)
+  .put(AuthMiddleware, updateRecipient)
+  .delete(AuthMiddleware, deleteRecipient);
 
 export default RecipientRouter;

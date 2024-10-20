083ff02cb829552ad5211cdfd3062b5581763c32
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-20 09:55:39 +0300

    Add logout functionality to Sidebar component and update icon

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,9 +1,18 @@
 // components/Sidebar.js
-import { LayoutDashboard, Activity, Settings, Users } from "lucide-react";
+import {
+  LayoutDashboard,
+  Activity,
+  Settings,
+  Users,
+  LogOut,
+} from "lucide-react";
+import { useUser } from "@/hooks/useUser";
 import { MdBloodtype } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  const { logOut } = useUser();
+
   return (
     <aside className="w-64 bg-white shadow-md">
       <div className="p-4">
@@ -40,11 +49,11 @@ const Sidebar = () => {
             Blood Types
           </Link>
           <Link
-            to="/settings"
-            className="flex items-center px-4 py-2 mt-2 text-gray-600 hover:bg-gray-200 rounded-md"
+            onClick={logOut}
+            className="flex items-center px-4 mt-[400px] py-2 text-gray-600 hover:bg-gray-200 rounded-md"
           >
-            <Settings className="mr-3 h-5 w-5" />
-            Settings
+            <LogOut className="mr-3 h-5 w-5" />
+            Logout
           </Link>
         </nav>
       </div>

3426108de693a9f5c23a1c70bfd5127256f699f8
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-22 21:38:49 +0300

    fix: apply AuthMiddleware to login route in AdminRoutes

--- server/routes/AdminRoutes.js
+++ server/routes/AdminRoutes.js
@@ -7,11 +7,10 @@ import { deleteAdmin } from "../controllers/Admin/DeleteAdminController.js";
 import { AuthMiddleware } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
-
 router.post("/", RegisterAdmin);
-router.post("/login", loginAdmin);
-router.get("/data",GetAdminData);
-router.put("/:id", AuthMiddleware,UpdateAdmin);
-router.delete("/:id", AuthMiddleware,deleteAdmin);
+router.post("/login", AuthMiddleware, loginAdmin);
+router.get("/data", GetAdminData);
+router.put("/:id", AuthMiddleware, UpdateAdmin);
+router.delete("/:id", AuthMiddleware, deleteAdmin);
 
 export default router;

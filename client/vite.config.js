544ca99560816192f2c6326af47b9820ca52b856
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-22 19:22:41 +0300

    fix: update API proxy URL to point to production server

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:22000",
+      "/api": "https://api-badbaadiye.vercel.app/",
     },
   },
 });

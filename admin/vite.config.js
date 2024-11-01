c1eb0a28a29a80930dd712f9b688b6e37cd0379b
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-01 19:37:13 +0300

    feat: update Vite config and package.json for improved server setup and new dependencies

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:22000",
+      "/api": "http://localhost:22000",
     },
   },
   resolve: {

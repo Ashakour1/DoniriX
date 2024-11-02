58a4debae8d96d1667e9a55d2f05a35a9fb0725e
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-02 17:35:37 +0300

    feat: enhance generateDonorId function to ensure unique donor IDs with database check

--- server/utils/generate-id.js
+++ server/utils/generate-id.js
@@ -1,14 +1,30 @@
+import prisma from "../config/prisma.js";
+
 let referenceNumber = 0; // Initialize reference number
 
-const generateDonorId = () => {
+const generateDonorId = async () => {
   const date = new Date();
   const month = String(date.getMonth() + 1).padStart(2, "0"); // Get month (01-12)
   const year = String(date.getFullYear()).slice(-2); // Get last two digits of the year
-  const ref = String(referenceNumber++).padStart(2, "0"); // Increment reference number
 
-  // console.log(date,ref, year, month);
+  let donorId;
+  let isUnique = false;
 
-  return `DX${month}${year}${ref}`; // Create ID
+  while (!isUnique) {
+    const ref = String(referenceNumber++).padStart(2, "0"); // Increment reference number
+    donorId = `DX${month}${year}${ref}`;
+
+    // Check if this donorId already exists in the database
+    const existingDonor = await prisma.donar.findUnique({
+      where: { donorId },
+    });
+
+    if (!existingDonor) {
+      isUnique = true;
+    }
+  }
+
+  return donorId;
 };
 
 export default generateDonorId;

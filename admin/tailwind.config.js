d2ac27bb964f267a1c4723dc5c730d1453e656d8
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-03 08:53:57 +0300

    feat: add flowbite-react for enhanced UI components in admin panel

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this based on your project structure
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", flowbite.content()],
   theme: {
     extend: {
       colors: {
         Accent: "#005B96",
-
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
